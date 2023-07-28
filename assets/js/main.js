234ba2288fa055dccf719b8179c4ea4714cf57d5
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-07-28 12:09:42 +0100

    Add dropdown feature to navbar and link artisan safety and client safety page to all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const navDropdown = primaryNav.querySelector(".header__nav--dropdown");
 
 navToggle.addEventListener("click", toggleNav);
 
@@ -24,3 +25,24 @@ function closeNav(event) {
 }
 
 window.addEventListener("click", closeNav);
+
+function toggleNavDropdown() {
+  const navDropdownMenu = event.currentTarget.querySelector("ul");
+  const isSelected = navDropdown.getAttribute("aria-selected");
+  if (isSelected === "true") {
+    navDropdown.setAttribute("aria-selected", "false");
+  } else {
+    navDropdown.setAttribute("aria-selected", "true");
+  }
+}
+
+navDropdown.addEventListener("click", toggleNavDropdown);
+
+function closeNavDropdown(event) {
+  const isOutside = !event.target.closest("header");
+  if (isOutside) {
+    navDropdown.setAttribute("aria-selected", "false"); 
+  }
+}
+
+window.addEventListener("click", closeNavDropdown);
